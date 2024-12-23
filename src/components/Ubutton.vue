e16e0ad19d96ce788c1387e3e7076f54cd6b5b0e
bilalkolli <bilal.kokoko@outlook.fr>
2024-12-23 04:09:07 +0100

    Feat: add Button component

---
+++ src/components/Ubutton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+type Opacity = 'bg-opacity-10' | 'bg-opacity-100'
+
+defineProps<{
+  label: string,
+  opacity: Opacity
+}>()
+</script>
+
+<template>
+  <Button :class="`rounded-xl bg-customPink ${opacity}`">
+    {{ label }}
+  </Button>
+</template>
