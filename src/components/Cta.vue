8aadc5462db63d49ddb182f0d9151605fc8a6db7
bilalkolli <bilal.kokoko@outlook.fr>
2024-12-26 01:17:25 +0100

    Feat: add Cta component

---
+++ src/components/Cta.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue"
+
+defineProps<{
+  promotion: number
+}>()
+</script>
+
+<template>
+    <div class="flex bg-[url('/BG.webp')] bg-cover bg-left rounded-lg">
+      <div class="py-16 pl-20 rounded-lg">
+        <h2 class="text-3xl custom-font-bold mb-4">
+          {{ promotion }}% Discount!
+        </h2>
+        <p class="text-xl max-w-xs mb-4">Get a discount on certain days and don’t miss it.</p>
+        <Icon 
+          class="rounded-full p-1 bg-customPink" 
+          icon="system-uicons:arrow-top-right" 
+          width="2em" 
+          height="2em" 
+          color="white"
+        />
+      </div>
+      <img 
+        class="ml-auto" 
+        src="/image.svg"  
+        alt="Image building call to Action"
+      >
+    </div>
+</template>
