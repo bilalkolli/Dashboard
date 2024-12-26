1037746f8fbb25b77551d6440354a28cccd33085
bilalkolli <bilal.kokoko@outlook.fr>
2024-12-26 02:41:21 +0100

    feat(layout): add Dashboard component to the main layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
+import Dashboard from './components/Dashboard.vue'
 </script>
 
 <template>
-    <main class=" min-h-screen text-dark-purple flex">
+    <main class=" min-h-screen text-dark-purple bg-light-gray flex justify-between custom-font">
         <Navbar />
+        <Dashboard />
     </main>
 </template>
